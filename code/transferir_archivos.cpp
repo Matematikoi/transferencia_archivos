5b12ae314352a8754d135c807ebc12a602f9d053
Gabriel Octavio Lozano Pinzón <golozanop@unaledu.co>
2020-05-19 17:47:45 -0500

    primera version estable

--- code/transferir_archivos.cpp
+++ code/transferir_archivos.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 using namespace std;
 int * BASE,* RECIBIDO;
 void llenar_base(int tamano_arreglo, int peso_archivo){
@@ -9,24 +12,57 @@ void llenar_base(int tamano_arreglo, int peso_archivo){
     }
     memset(RECIBIDO,0,peso_archivo);
 }
+int guardar_base(int peso_archivo){
+    FILE *apFile;
+    int r;
+    apFile = fopen("binaries/base.bin","w+");
+    if(apFile == NULL){
+        perror("error fopen:");
+        exit(-1);
+    }
+    r = fwrite(BASE, peso_archivo,1, apFile);
+    if(r <= 0){
+        perror("error fwrite");
+        exit(-1);
+    }
+    r = fclose(apFile);
+    if(r < 0){
+        perror("error fclose: ");
+        exit(-1);
+    }
+    return 1;
+}
 
+void leer_base_a_recibido(int peso_archivo){
+    ifstream archivo ("binaries/base.bin", ios::in|ios::binary|ios::ate);
+    archivo.seekg (0, ios::beg);
+    archivo.read ( (char*) RECIBIDO ,peso_archivo);
+    archivo.close();
+}
 
 int main(int argc, char* argv[]){
-    // Comprobamos el numero de parametros
-    if (argc < 2) {
-        cerr << "Uso: " << argv[0] << "n  c" << endl;
-        cerr << "donde n es el numero y c un caracter. Para expresar \n";
-        cerr << "100Kb se debe escribir 100 k "<<endl;
-        return 1;
-    }
     map <string,int> prefijos;
     prefijos["k"]=1e3;
     prefijos["m"]=1e6;
     int peso_archivo=prefijos[argv[2]]*stoi(argv[1]);
     int tamano_arreglo = peso_archivo/8;
-    cerr<<"tamano de arreglo : "<<tamano_arreglo<<endl;
+    cout<<"tamano de arreglo : "<<tamano_arreglo<<endl;
     llenar_base(tamano_arreglo, peso_archivo);
-    for (int i =0;i<tamano_arreglo;++i)
-        cout<<BASE[i]<<' ' <<RECIBIDO[i]<<endl;
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid == -1) { 
+      perror("fork"); exit(EXIT_FAILURE); 
+    }
+    if (pid == 0){
+        guardar_base(peso_archivo);
+    }
+    else{
+        if (wait(&status) == pid){
+            leer_base_a_recibido(peso_archivo);
+        }
+    }
     
+    cout<<"finalizado"<<endl;
+
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
 g++ code/transferir_archivos.cpp -o binaries/transferir.bin
+echo "Archivos" > resultados
+
+for prefijo in k m 
+do 
+	for valor in 1 10
+	do
+		(time  ./binaries/transferir.bin $valor $prefijo ) > out 2> tiempo
+		echo -n "$valor$prefijo " >>resultados
+		cat tiempo | sed -n 2p | sed 's/\t/ /g' | sed 's/,/./g' | cut -d ' ' -f 2 >> resultados
+	done
+done
