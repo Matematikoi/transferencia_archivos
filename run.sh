35bc020ae84b2cb99b935a0323a18b0b3746206d
Gabriel Octavio Lozano Pinzón <golozanop@unaledu.co>
2020-05-20 00:47:18 -0500

    se añaden las imagenes

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ do
 	for valor in 1 10 100
 	do
 		(time  ./binaries/transferir.bin $valor $prefijo ) > out 2> tiempo
-		echo -n "$valor$prefijo " >>resultados
+		echo -n "$valor$prefijo, " >>resultados
 		cat tiempo | sed -n 2p | sed 's/\t/ /g' | sed 's/,/./g' | cut -d ' ' -f 2 >> resultados
 	done
 done
@@ -21,7 +21,7 @@ do
 	for valor in 1 10 100
 	do
 		(time  ./binaries/memoria.bin $valor $prefijo ) > out 2> tiempo
-		echo -n "$valor$prefijo " >>resultados
+		echo -n "$valor$prefijo ," >>resultados
 		cat tiempo | sed -n 2p | sed 's/\t/ /g' | sed 's/,/./g' | cut -d ' ' -f 2 >> resultados
 	done
 done
