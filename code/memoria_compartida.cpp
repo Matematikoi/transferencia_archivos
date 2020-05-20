5d0d2eee9e44fdecae83dc7e7f4becd75d722936
Gabriel Octavio Lozano Pinzón <golozanop@unaledu.co>
2020-05-19 19:37:13 -0500

    commit de afan

---
+++ code/memoria_compartida.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+using namespace std;
+int * BASE,* RECIBIDO;
+void llenar_base(int tamano_arreglo, int peso_archivo){
+    BASE = (int*) malloc(peso_archivo+1);
+    RECIBIDO = (int*) malloc(peso_archivo+1);
+    for (int i = 0 ;i <tamano_arreglo;++i){
+        BASE[i] = i % 1000;
+    }
+    memset(RECIBIDO,0,peso_archivo);
+}
+
+
+
+int main(int argc, char* argv[]){
+    map <string,int> prefijos;
+    prefijos["k"]=1e3;
+    prefijos["m"]=1e6;
+    int peso_archivo=prefijos[argv[2]]*stoi(argv[1]);
+    int tamano_arreglo = peso_archivo/8;
+    cout<<"tamano de arreglo : "<<tamano_arreglo<<endl;
+    llenar_base(tamano_arreglo, peso_archivo);
+
+    pid_t pid;
+    int status,shmId, *ap;
+    key_t key=9192;
+    //crear espacio de memoria compartida
+    shmId = shmget(key, peso_archivo, 0666|IPC_CREAT);
+    if(shmId < 0){
+        perror("Error en shmget");
+        exit(-1);
+    }
+    //asociar espacio de memoria creada a este proceso
+    ap = (int *)shmat(shmId, 0, 0);
+    
+    
+    pid = fork();
+    if (pid == -1) { 
+      perror("error en fork"); exit(EXIT_FAILURE); 
+    }
+    if (pid == 0){
+        for (int i=0;i<tamano_arreglo;++i)
+            *(ap+i) = BASE[i];
+    }
+    else{
+        if (wait(&status) == pid){
+            for (int i=0;i<tamano_arreglo;++i)
+                RECIBIDO[i] = *(ap+i);
+        }
+    }
+ 
+    for (int i=0;i<tamano_arreglo;++i){
+        //cout << BASE[i]<<' '<<RECIBIDO[i]<<endl;
+        continue;
+    }
+    int r = shmdt(ap);  //desasociar espacio de memoria compartida
+    if(r < 0){
+      perror("error shmdt ");
+      exit(-1);
+    }
+
+}
--- run.sh
+++ run.sh
@@ -3,9 +3,24 @@ echo "Archivos" > resultados
 
 for prefijo in k m 
 do 
-	for valor in 1 10
+	for valor in 1 10 100
 	do
 		(time  ./binaries/transferir.bin $valor $prefijo ) > out 2> tiempo
+		echo -n "$valor$prefijo " >>resultados
+		cat tiempo | sed -n 2p | sed 's/\t/ /g' | sed 's/,/./g' | cut -d ' ' -f 2 >> resultados
+	done
+done
+
+
+echo "memoria_compartida" >> resultados
+
+g++ code/memoria_compartida.cpp -o binaries/memoria.bin
+./binaries/memoria.bin 100 m 
+for prefijo in k m 
+do 
+	for valor in 1 10 100
+	do
+		(time  ./binaries/memoria.bin $valor $prefijo ) > out 2> tiempo
 		echo -n "$valor$prefijo " >>resultados
 		cat tiempo | sed -n 2p | sed 's/\t/ /g' | sed 's/,/./g' | cut -d ' ' -f 2 >> resultados
 	done
